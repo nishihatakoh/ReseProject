d004bb36b6b9cc63560070e67c26600e9de19795
nishihatakoh <koh.s13.kk@gmail.com>
2022-09-10 20:10:29 +0900

    fix

--- app/Models/admin.php
+++ app/Models/admin.php
@@ -12,7 +12,7 @@ class Admin extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    protected $guard = 'Admins';
+    protected $guard = 'Admin';
 
     /**
      * The attributes that are mass assignable.
