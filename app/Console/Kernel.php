c9e2cc42fbcf084b244c0d5a48efb906d8c80586
nishihatakoh <koh.s13.kk@gmail.com>
2022-09-14 00:05:04 +0900

    fix

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('ReserveMail')->at('0:00');
+        $schedule->command('ReserveMail')->at('0:10');
     }
 
     /**
